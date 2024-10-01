7424eb9d185121a364099de39ef6e0d43c53d8be
Federico Nicolas Bouvier <136862169+federiconicolasbouvier@users.noreply.github.com>
2024-10-01 18:24:37 -0300

    Add files via upload

---
+++ Project4.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bouvier.project4;
+
+/**
+ *
+ * @author Federico Nicolás Bouvier
+ */
+public class Project4 {
+
+    public static void main(String[] args) {
+        Project4GUI ventana = new Project4GUI();
+        ventana.setVisible(true);
+    }
+}
